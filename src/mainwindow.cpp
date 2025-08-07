3630d2b75012eb7276dbb47a7df368ae10ee7424
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-07 11:20:07 +0200

    Start with basic application

---
+++ src/mainwindow.cpp
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileComment: Project Homepage: https://github.com/rstammdev/comma-qt
+ * SPDX-FileCopyrightText: 2025 Ralf Stamm
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "mainwindow.h"
+
+using namespace Qt::Literals::StringLiterals;
+
+
+MainWindow::MainWindow(QWidget* parent)
+    : QMainWindow(parent)
+{}
+
+MainWindow::~MainWindow() {}
